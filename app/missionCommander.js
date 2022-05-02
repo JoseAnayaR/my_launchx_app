eb499da90b6e08cad1be898c3aa85656af791619
JoseAnayaR <joseanayautj@gmail.com>
2022-05-02 00:25:09 -0500

    Añadir primeros documentos y prueba base jest

---
+++ app/missionCommander.js
@@ -0,0 +1,7 @@
+class MissionCommander {
+    constructor(name){
+        this.name = name
+    }
+}
+//Con esta linea exportamos la clase
+module.exports = MissionCommander
---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const jose = new MissionCommander("Jose")
+const flor = new MissionCommander("Flor")
+console.log(carlo.name)
+console.log(jose.name)
+console.log(flor.name)
